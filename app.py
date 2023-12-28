b5bc699df3a08bd9d0d69706d1dcdbd8edf748d6
Shivansh <shivraj7303@gmail.com>
2023-12-28 10:52:14 +0530

    update app.py, output.css, contact.html and home.html

--- app.py
+++ app.py
@@ -1,19 +1,48 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route('/')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/quillblog"
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(30), nullable=False)
+    Email = db.Column(db.String(30), nullable=False)
+    Message = db.Column(db.String(200), nullable=False)
+
+
+@app.route("/")
 def home():
-    return render_template("home.html", home = True)
-@app.route('/post')
+    return render_template("home.html", home=True)
+
+
+@app.route("/post")
 def post():
     return render_template("post.html")
-@app.route('/about')
+
+
+@app.route("/about")
 def about():
     return render_template("about.html")
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
 
-if __name__ == '__main__':
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form.get("name")
+        email = request.form.get("email")
+        message = request.form.get("message")
+
+        entry = Contact(Name=name, Email=email, Message=message)
+        db.session.add(entry)
+        db.session.commit()
+
+    return render_template("contact.html", msg=True)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
